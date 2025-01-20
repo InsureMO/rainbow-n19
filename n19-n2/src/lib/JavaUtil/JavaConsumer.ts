d8fcc44cf5cb13cf425a2d79e63cd4f52fe2b019
brad.wu <brad.wu@ebaotech.com>
2025-01-20 22:30:50 +0800

    init

--- n19-n2/src/lib/JavaUtil/JavaConsumer.ts
+++ n19-n2/src/lib/JavaUtil/JavaConsumer.ts
@@ -1,9 +1,23 @@
-import {AndThenJavaConsumer} from './AndThenJavaConsumer';
-
 export abstract class JavaConsumer<T> {
 	abstract accept(t: T): void;
 
 	andThen(after: JavaConsumer<T>): JavaConsumer<T> {
 		return new AndThenJavaConsumer(this, after);
+	}
+}
+
+export class AndThenJavaConsumer<T> extends JavaConsumer<T> {
+	private readonly _apply: JavaConsumer<T>;
+	private readonly _after: JavaConsumer<T>;
+
+	constructor(apply: JavaConsumer<T>, after: JavaConsumer<T>) {
+		super();
+		this._apply = apply;
+		this._after = after;
+	}
+
+	accept(t: T): void {
+		this._apply.accept(t);
+		this._after.accept(t);
 	}
 }
--- n19-n2/src/lib/JavaUtil/index.ts
+++ n19-n2/src/lib/JavaUtil/index.ts
@@ -1,6 +1,2 @@
-export * from './AndThenJavaConsumer';
-export * from './AndThenJavaFunction';
-export * from './IdentityJavaFunction';
 export * from './JavaConsumer';
 export * from './JavaFunction';
-export * from './WithBeforeJavaFunction';
