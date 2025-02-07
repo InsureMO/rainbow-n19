fdda92fa884d0ec9b04c4471a8cfc323de15fad6
brad.wu <brad.wu@ebaotech.com>
2025-02-07 11:20:12 +0800

    init

--- n19-n2/test/ClassLoader.spec.ts
+++ n19-n2/test/ClassLoader.spec.ts
@@ -1,26 +1,46 @@
-import {Class, Constructor, JREClassLoader, Method, Modifier, Parameter} from '../src/lib/Java';
+import {Class, ClassCreateHelper, Constructor, JREClassLoader, Method, Modifier, Parameter} from '../src/lib/Java';
 
 describe('ClassLoader Test', () => {
 	const classLoader = new JREClassLoader('17');
 	test('Add object class', async () => {
-		const clazz = new Class(classLoader, {
-			name: 'java.lang.Object',
-			modifiers: Modifier.PUBLIC,
-			declaredConstructors: (declaringClass) => {
-				return [new Constructor(declaringClass, {modifiers: Modifier.PUBLIC})];
-			},
-			declaredMethods: (declaringClass) => {
-				return [new Method(declaringClass, {
-					name: 'equals',
-					modifiers: Modifier.PUBLIC,
-					parameters: (executable) => {
-						return [new Parameter(executable, {name: 'obj', typeOrName: 'java.lang.Object'})];
-					},
-					returnedTypeOrName: 'boolean'
-				})];
-			}
-		});
-		classLoader.addClass(clazz);
+		// const clazz = new Class(classLoader, {
+		// 	name: 'java.lang.Object',
+		// 	modifiers: Modifier.PUBLIC,
+		// 	declaredConstructors: (declaringClass) => {
+		// 		return [new Constructor(declaringClass, {modifiers: Modifier.PUBLIC})];
+		// 	},
+		// 	declaredMethods: (declaringClass) => {
+		// 		return [new Method(declaringClass, {
+		// 			name: 'equals',
+		// 			modifiers: Modifier.PUBLIC,
+		// 			parameters: (executable) => {
+		// 				return [new Parameter(executable, {name: 'obj', typeOrName: () => 'java.lang.Object'})];
+		// 			},
+		// 			returnedTypeOrName: () => 'boolean'
+		// 		})];
+		// 	}
+		// });
+		// classLoader.addClass(clazz);
+		const udf = (void 0);
+		// noinspection JSConsecutiveCommasInArrayLiteral
+		ClassCreateHelper.on(classLoader)
+			.class('java.lang.Object', [
+				/* super class */,
+				/* interfaces */,
+				Modifier.PUBLIC,
+				/* declared annotations */,
+				/* type parameters */,
+				[ /* declared constructors */
+					[/* parameters */, /* throwns */, Modifier.PUBLIC, /* declared annotations */, /* type parameters */udf]
+				],
+				[ /* declared methods */
+					['equals', [ /* parameters */
+						['obj', 'java.lang.Object', Modifier.PUBLIC, /* declared annotations */ udf]
+					], 'boolean', /* throwns */, Modifier.PUBLIC, /* declared annotations */, /* type parameters */udf]
+				],
+				/* declared fields */,
+				/* enum values */udf
+			]);
 		console.log(classLoader);
 	});
 });
