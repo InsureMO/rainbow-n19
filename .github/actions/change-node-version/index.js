b7be6905bb8bbba5bc74c781963173eb673a3454
brad.wu <brad.wu@ebaotech.com>
2025-03-19 10:36:38 +0800

    init

--- .github/actions/change-node-version/index.js
+++ .github/actions/change-node-version/index.js
@@ -9,7 +9,7 @@ const modifyPackageJson = (moduleName, packageFile, version) => {
 	['dependencies', 'devDependencies'].forEach(key => {
 		if (packageJson[key] != null) {
 			Object.keys(packageJson[key]).forEach(name => {
-				if (name.startsWith('@rainbow-o23/')) {
+				if (name.startsWith('@rainbow-n19/')) {
 					packageJson[key][name] = version;
 				}
 			});
@@ -17,16 +17,4 @@ const modifyPackageJson = (moduleName, packageFile, version) => {
 	});
 	const newContent = JSON.stringify(packageJson, null, '\t');
 	fs.writeFileSync(packageFile, newContent, 'utf8');
-}
-try {
-	const moduleName = core.getInput('module-name');
-	const version = core.getInput('target-version');
-	const packageFile = `./${moduleName}/package.json`;
-	modifyPackageJson(moduleName, packageFile, version);
-	if (moduleName === 'create-rainbow-o23-app') {
-		const templatePackageFile = `./${moduleName}/templates/package.json`;
-		modifyPackageJson(moduleName, templatePackageFile, version);
-	}
-} catch (error) {
-	core.setFailed(error.message);
 }
