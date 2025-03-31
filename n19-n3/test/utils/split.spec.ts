2f8b783190d4ad18c80be15de74159e4973e92a6
brad.wu <brad.wu@ebaotech.com>
2025-03-31 18:24:59 +0800

    init

--- n19-n3/test/utils/split.spec.ts
+++
@@ -1,8 +0,0 @@
-describe('Split string', () => {
-	test('Split string', async () => {
-		const x = new Array(999999).fill(1).map(() => 'a').join('');
-		console.time('aaa');
-		const y = x.split('');
-		console.timeEnd('aaa');
-	});
-});
