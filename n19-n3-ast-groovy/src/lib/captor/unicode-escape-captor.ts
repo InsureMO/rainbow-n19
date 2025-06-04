d62dadc52998cf145bcbdf5bccb166a2511a51d0
brad.wu <brad.wu@ebaotech.com>
2025-06-04 14:57:19 +0800

    init

--- n19-n3-ast-groovy/src/lib/captor/unicode-escape-captor.ts
+++ n19-n3-ast-groovy/src/lib/captor/unicode-escape-captor.ts
@@ -1,41 +1,34 @@
 import {TokenId, TokenType} from '../tokens';
-import {AbstractMultipleCharsCaptor} from './abstract-multiple-chars-captor';
+import {AbstractAstNodeCaptor} from './abstract-captor';
 import {AstTokenizer} from './ast-tokenizer';
 import {AstNodeCaptorCheckers} from './captor';
 import {isHexadecimalNumeric} from './captor-func-checkers';
 import {Char} from './types';
 
-export class LtUnicodeEscapeCaptor extends AbstractMultipleCharsCaptor {
+export class LtUnicodeEscapeCaptor extends AbstractAstNodeCaptor {
 	private readonly _checker: AstNodeCaptorCheckers;
 
 	constructor() {
-		super('\\u');
-		this._checker = [...this.charsArray, isHexadecimalNumeric, isHexadecimalNumeric, isHexadecimalNumeric, isHexadecimalNumeric];
-	}
-
-	protected get keyword(): string {
-		return this.chars;
+		super();
+		this._checker = ['\\', 'u', isHexadecimalNumeric, isHexadecimalNumeric, isHexadecimalNumeric, isHexadecimalNumeric];
 	}
 
 	checkers(): AstNodeCaptorCheckers {
 		return this._checker;
 	}
 
-	protected getTokenNature(): [TokenId, TokenType] {
-		return [TokenId.StringUnicodeEscape, TokenType.StringLiteral];
-	}
-
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	visit(_char: Char, offset: number, tokenizer: AstTokenizer): void {
-		const [tokenId, tokenType] = this.getTokenNature();
 		const text = [
-			this.chars,
+			'\\u',
 			tokenizer.charAt(offset + 2),
 			tokenizer.charAt(offset + 3),
 			tokenizer.charAt(offset + 4),
 			tokenizer.charAt(offset + 5)
 		].join('');
-		this.createAndAppendToAst(tokenizer, {tokenId, tokenType, text, startOffset: offset});
+		this.createAndAppendToAst(tokenizer, {
+			tokenId: TokenId.StringUnicodeEscape, tokenType: TokenType.StringLiteral, text, startOffset: offset
+		});
 		tokenizer.moveCursorTo(offset + 6);
 	}
 }
